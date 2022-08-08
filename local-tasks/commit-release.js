03747d0aa853d1fef170fa40a6904b48ba43d204
Davyd McColl <davydm@gmail.com>
2022-08-08 13:52:30 +0200

    :bookmark: release version 1.0.2

---
+++ local-tasks/commit-release.js
@@ -0,0 +1,42 @@
+const 
+  gulp = requireModule("gulp-with-help"),
+  chalk = require("chalk"),
+  logger = requireModule("log"),
+  editXml = require("gulp-edit-xml"),
+  Git = require("simple-git"),
+  git = new Git(),
+  env = requireModule("env"),
+  containingFolder = "src/Quackers.TestLogger";
+
+// TODO: move up into gulp-tasks as a generic that can take
+// in a gulp.src stream
+gulp.task("commit-release", () => {
+  return new Promise((resolve, reject) => {
+    gulp.src(`${containingFolder}/Quackers.TestLogger.csproj`).pipe(
+      editXml(xml => {
+        const packageVersionPropGroup = xml.Project.PropertyGroup.filter(
+            g => !!g.PackageVersion
+          )[0],
+          node = packageVersionPropGroup.PackageVersion,
+          version = node[0].trim();
+
+        logger.notice(chalk.cyanBright(`Committing release ${version}`));
+        if (env.resolveFlag("DRY_RUN")) {
+          resolve(" -- dry run: no commit --");
+          return xml;
+        }
+        git.add("./*", err => {
+          if (err) {
+            reject(`Unable to add all files: ${err}`);
+          }
+          git.commit(`:bookmark: release version ${version}`, err => {
+            return err
+              ? reject(`Unable to commit release ${version}: ${err}`)
+              : resolve(`Release ${version} committed`);
+          });
+        });
+        return xml;
+      })
+    );
+  });
+});

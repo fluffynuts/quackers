515a7ac3da05a623f38fc9a3af357ce688476b40
Davyd McColl <davydm@gmail.com>
2023-09-13 12:22:08 +0200

    :arrow_up: upgrade test host to net7.0

---
+++ local-tasks/test-zarro.ts
@@ -0,0 +1,15 @@
+/// <reference path="../node_modules/zarro/types.d.ts" />
+(function () {
+
+  const
+    gulp = requireModule<Gulp>("gulp"),
+    testZarro = requireModule<TestZarro>("test-zarro");
+
+  gulp.task("test-zarro", async () => {
+    await testZarro({
+      package: "beta",
+      rollback: true,
+      tasks: ["test", "pack"]
+    });
+  });
+})();
